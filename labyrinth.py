e4f7750c779ab8a89b429bcd89a1cb02b1441c6b
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-08-27 20:27:51 +0000

    Add futher riddles into the riddles.py list of dictionaries. Continue writing story narrative with end of first chapter and beginning of second chapter.

--- labyrinth.py
+++ labyrinth.py
@@ -1,3 +1,5 @@
+import sys
+from time import sleep
 from riddles import riddle
 
 title = """
@@ -19,36 +21,132 @@ title = """
  """
 
 print(title)
-print("Greetings adventurer! \n")
-print("Welcome to the Labyrinth of Riddles. \n")
-print("Only the truly worthy can succeed in completing the Labyrinth. \n")
-print("Navigate the maze and answer the riddles correctly to prove yourself")
-print("worthy \n")
-print("But beware! Many a brave explorer has become lost and mad within its")
-print("walls. \n")
+intro_paragraph = """
+Greetings adventurer!
+
+Welcome to the Labyrinth of Riddles.
+
+Only the truly worthy can succeed in completing the Labyrinth.
+
+Navigate the maze and answer the riddles correctly to prove yourself
+worthy.
+
+Legend has it that all the riddles have one word answers..
+
+But beware! Many brave explorers who venture into the labyrinth never return.
+"""
+print(intro_paragraph)
 
 play_game = True
 
+def second_riddle():
+    print("Second Riddle.")
+
+def second_chapter():
+    second_paragraph = """
+--------------------------------------------------------------
+
+"You guessed correctly... I'm impressed. It's been a thousand years since
+someone solved one of my riddles. Maybe you will finally be a worthy 
+opponent?"    
+
+"But don't get cocky. I always save my hardest riddles for last!"
+
+"I'll see you soon... if you survive."
+
+There's a blinding flash of light. You shade your eyes and the Riddler
+vanishes from sight.
+"""
+    print(second_paragraph)
+    path_two = """
+Infront of you there are two pathways.
+
+One path goes left, and the other goes right.
+
+The ends of both paths are obscured from view.
+
+There is a small carving in the stone wall infront of you.
+
+You lean in to have a closer look.
+
+It reads "Whatever you do, don't go left."
+"""
+    print(path_two)
+    while play_game:
+        choice = input("Which path will you choose? (left/right) \n").lower().strip()
+        if choice == "right" or choice == "r":
+            second_riddle()
+            break
+        elif choice == "left" or choice == "l":
+            left = """
+--------------------------------------------------------------
+
+Against your better judgement you decide to ignore the words of warning
+and go left anyway.
+
+You follow the pathway forwards and then around to the left. 
+
+You begin to notice small holes in the ceiling and walls as you continue to 
+walk forwards.
+
+As you walk further into the passageway, you hear an audible click as one 
+of your feet touches a panel on the floor.
+
+"Ah crap, why did I go left?!"
+
+Before you have time to react, you hear the whip of hundreds of arrows
+as they are released from the holes in the walls and ceiling and whizz towards
+your body.
+
+It all goes black.
+
+GAME OVER.
+            """
+            print(left)
+            while play_game:
+                start = input("Do you wish to play again? (yes/no) \n").lower().strip()
+                if start == "yes" or start == "y":
+                    print("\nLet it begin! \n")
+                    begin_labyrinth()
+                    break
+                elif start == "no" or start == "n":
+                    quit()
+                else:
+                    print("Invalid input. Please try again. \n")
+        else:
+            print("Invalid input. Please try again. \n")
+
+
 def you_die():
     final_words = """
+--------------------------------------------------------------
+
 "Your three guesses are up." 
 
 The Ridder raises his hand.
 
-"BE GONE!"
+"You are not worthy... YOU FAILED! BE GONE!"
 
-There is a bright flash and everything fades to black
+There is a bright flash and everything fades to black...
 
 GAME OVER.
     """
     print(final_words)
-    quit()
+    while play_game:
+        start = input("Would you like to try again? (yes/no) \n").lower().strip()
+        if start == "yes" or start == "y":
+            print("Let it begin! \n")
+            begin_labyrinth()
+            break
+        elif start == "no" or start == "n":
+            quit()
+        else:
+            print("Invalid input. Please try again. \n")
 
 def first_riddle():
-    riddle_one = riddle[0]["riddle_one"]
+    riddle_one = riddle[0]["riddle"]
     riddle_one_answer = riddle[0]["correct"]
-    print(riddle_one_answer)
-    path_one = f"""
+    path_one = """
 --------------------------------------------------------------
 
 You walk forwards into the path leading straight into the labyrinth.
@@ -61,22 +159,24 @@ The figure speaks in a quiet raspy voice: "I am the one they call... The Ridder.
 
 "If you cannot guess my riddle in three, you die."
 
-The figure points at you.
+The Riddler points at you.
 
 "Riddle me this."
-
-"{riddle_one}"
-    
     """
     print(path_one)
+    for char in riddle_one:
+        sleep(0.05)
+        sys.stdout.write(char)
+        sys.stdout.flush()
     guesses = 3
     while play_game:
-        choice = input(" \n").lower().strip()
+        print(f"\nYou have {guesses} guesses left. \n")
+        choice = input("What is your guess? \n").lower().strip()
         if choice == riddle_one_answer:
-            print("Correct")
+            second_chapter()
             break
         else:
-            print("Incorrect")
+            print('\n"You guessed incorrectly. Try again"')
             guesses -= 1
             if guesses == 0:
                 you_die()
@@ -100,7 +200,6 @@ The stones are old and sticky to the touch.
 
 Before you are two paths. One path continues straight and the other follows 
 the outer ring of the labyrinth to the left.
-
 """
     
     print(first_paragraph)
@@ -125,7 +224,12 @@ You have reached a dead end.
                 dead_end_choice = input("Do you want to return back \
 or do you want to exit the labyrinth (back/exit) \n").lower().strip()
                 if dead_end_choice == "back" or dead_end_choice == "b":
-                    print("You retrace your steps back to where you were. \n")
+                    back = """
+--------------------------------------------------------------
+
+You retrace your steps back to where you were.
+                    """
+                    print(back)
                     break
                 elif dead_end_choice == "exit" or dead_end_choice == "e":
                     quit()
@@ -137,7 +241,7 @@ or do you want to exit the labyrinth (back/exit) \n").lower().strip()
 while play_game:
     start = input("Do you wish to enter the Labyrinth of Riddles? (yes/no) \n").lower().strip()
     if start == "yes" or start == "y":
-        print("Let it begin! \n")
+        print("\nLet it begin! \n")
         begin_labyrinth()
         break
     elif start == "no" or start == "n":
--- riddles.py
+++ riddles.py
@@ -1,8 +1,46 @@
 riddle = [
     {
-        "riddle_one": """What can run but never walks; has a \
+        "riddle": '''"What can run but never walks; has a \
 mouth, but never talks; has a head, but never weeps; \
-has a bed, but never sleeps?""",
+has a bed, but never sleeps?"\n''',
         "correct": "river"
+    },
+    {
+        "riddle": '''"Halo of water, tongue of wood, skin of stone \
+long I've stood. My fingers short reach to the sky, inside my heart \
+men live and die. What am I?"\n''',
+        "correct": "castle"
+    },
+    {
+        "riddle": '''"First think of the person who lives in disguise, \
+who deals in secrets and tells naught but lies. Next, tell me what's always \
+the last thing to mend, the middle of middle and the end of the end? Finally, \
+give me the sound often heard during the search for a hard-to-find word. \
+Now, string them together, and answer me this: Which creature would you \
+be unwilling to kiss?"\n''',
+        "correct": "spider"
+    },
+    {
+        "riddle": '''"What is old and sometimes new; never sad, sometimes \
+blue; never empty, but sometimes full; never pushes, always pulls?"\n''',
+        "correct": "moon"
+    },
+    {
+        "riddle": '''"I am a word of letters three; add two and fewer \
+there will be. What word am I?"\n''',
+        "correct": "few"
+    },
+    {
+        "riddle": '''"Reaching stiffly for the sky, I bare my fingers \
+when it's cold. In warmth I wear an emerald glove and in between I \
+dress in gold. What am I?"\n''',
+        "correct": "tree"
+    },
+    {
+        "riddle": '''"What does man love more than life, hate more \
+than death or mortal strife; that which contented men desire; the \
+poor have, the rich require; the miser spends, the spendthrift saves, \
+and all men carry to their graves?"\n''',
+        "correct": "nothing"
     }
 ]
